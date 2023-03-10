9b460f69d37709f61923aa4af51af202bd37d313
Vaibhav A <72588823+techtheseus@users.noreply.github.com>
2023-03-10 10:24:16 +0530

    Added Swift UI Code

---
+++ Brainchime/Brainchime/BrainchimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BrainchimeApp.swift
+//  Brainchime
+//
+//  Created by whybhav on 10/03/23.
+//
+
+import SwiftUI
+
+@main
+struct BrainchimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
